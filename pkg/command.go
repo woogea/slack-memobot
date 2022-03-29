b1723c067f6093db399d72ed5e8a2886cb254903
woogy <rsswoogea@gmail.com>
2022-03-29 22:58:54 +0900

    add choice command

--- pkg/command.go
+++ pkg/command.go
@@ -1,6 +1,8 @@
 package mention
 
 import (
+	"fmt"
+	"math/rand"
 	"strings"
 )
 
@@ -13,7 +15,9 @@ func set(m *Mention, text string) (string, error) {
 	if len(items) < 1 {
 		return "", &InvalidArgumentError{"empty value"}
 	}
-	return "", m.storage.Add(items[0], strings.Join(items[1:], " "))
+	val := strings.Join(items[1:], " ")
+	val = strings.TrimSpace(val)
+	return "", m.storage.Add(items[0], val)
 }
 
 func get(m *Mention, text string) (string, error) {
@@ -30,6 +34,19 @@ func list(m *Mention, text string) (string, error) {
 	return "\n" + strings.Join(l, "\n"), nil
 }
 
+func rotate(m *Mention, text string) (string, error) {
+	return "", m.storage.Rotate(text)
+}
+
+func choice(m *Mention, text string) (string, error) {
+	l, err := m.storage.List(text)
+	if err != nil {
+		return "", err
+	}
+	// choice one item
+	return l[rand.Intn(len(l))], nil
+}
+
 func remove(m *Mention, text string) (string, error) {
 	items := strings.Split(text, " ")
 	if len(items) < 1 {
@@ -42,6 +59,14 @@ func removeAll(m *Mention, text string) (string, error) {
 	return "", m.storage.RemoveAll(text)
 }
 
+func funcList(m *Mention, text string) (string, error) {
+	ret := ""
+	for _, x := range m.command {
+		ret = ret + fmt.Sprintf("%s: %s\n", x.Name, x.Help)
+	}
+	return ret, nil
+}
+
 type Command struct {
 	Name      string
 	ShortName []string
@@ -49,41 +74,64 @@ type Command struct {
 	Help      string
 }
 
-var command = []Command{
-	{
-		Name:      "echo",
-		ShortName: []string{},
-		Action:    echo,
-		Help:      "reutrn input text",
-	},
-	{
-		Name:      "set",
-		ShortName: []string{"s"},
-		Action:    set,
-		Help:      "set <key> <value>",
-	},
-	{
-		Name:      "get",
-		ShortName: []string{"s"},
-		Action:    get,
-		Help:      "get <key>",
-	},
-	{
-		Name:      "list",
-		ShortName: []string{"l"},
-		Action:    list,
-		Help:      "list <key>",
-	},
-	{
-		Name:      "remove",
-		ShortName: []string{"r"},
-		Action:    remove,
-		Help:      "remove <key> <value>",
-	},
-	{
-		Name:      "removeAll",
-		ShortName: []string{"ra"},
-		Action:    removeAll,
-		Help:      "removeAll <key>",
-	},
+func (m *Mention) initCommand() {
+	commanddef := []Command{
+		{
+			Name:      "echo",
+			ShortName: []string{},
+			Action:    echo,
+			Help:      "reutrn input text",
+		},
+		{
+			Name:      "set",
+			ShortName: []string{"s"},
+			Action:    set,
+			Help:      "set <key> <value>",
+		},
+		{
+			Name:      "get",
+			ShortName: []string{"s"},
+			Action:    get,
+			Help:      "get <key>",
+		},
+		{
+			Name:      "list",
+			ShortName: []string{"l"},
+			Action:    list,
+			Help:      "list <key>",
+		},
+		{
+			Name:      "remove",
+			ShortName: []string{"r"},
+			Action:    remove,
+			Help:      "remove <key> <value>",
+		},
+		{
+			Name:      "rotate",
+			ShortName: []string{"rot"},
+			Action:    rotate,
+			Help:      "rotate <key>",
+		},
+		{
+			Name:      "choice",
+			ShortName: []string{"c"},
+			Action:    choice,
+			Help:      "choice <key>",
+		},
+		{
+			Name:      "removeAll",
+			ShortName: []string{"ra"},
+			Action:    removeAll,
+			Help:      "removeAll <key>",
+		},
+		{
+			Name:      "help",
+			ShortName: []string{"ra"},
+			Action:    funcList,
+			Help:      "help",
+		},
+	}
+	for _, x := range commanddef {
+		m.command = append(m.command, x)
+	}
 }
--- pkg/mention.go
+++ pkg/mention.go
@@ -1,22 +1,28 @@
 package mention
 
+import "strings"
+
 type Mention struct {
 	storage Storage
+	command []Command
 }
 
 func NewMention(namespace string) *Mention {
-	return &Mention{storage: NewMemoryStorage(namespace)}
+	ret := &Mention{storage: NewMemoryStorage(namespace)}
+	ret.initCommand()
+	return ret
 }
 
 func (m *Mention) Exec(cmd string, text string) (string, error) {
-	i := findFirst(command, cmd)
+	i := findFirst(m.command, cmd)
 	if i != -1 {
-		return command[i].Action(m, text)
+		return m.command[i].Action(m, text)
 	} else {
 		//exec get when the command is not found.
 		//return "", &NotfoundError{}
-		i := findFirst(command, "get")
-		return command[i].Action(m, cmd+" "+text)
+		i = findFirst(m.command, "choice")
+		text = strings.TrimSpace(cmd + " " + text)
+		return m.command[i].Action(m, text)
 	}
 }
 
